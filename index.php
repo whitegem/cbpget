e3a365cfc2620996541f24d6dd92f827b8e87a27
Jack Yu <root@jackyyf.com>
2012-12-26 21:21:48 +0800

    Initial Commit!

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+header('HTTP/1.1 403 Forbidden');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Configuration Required.</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>Configuration Required!</h1>
+<hr size="1" />
+<p>You need to redirect all process to processor.php!</p>
+</body>
+</html>
